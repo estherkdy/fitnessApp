d34769af4d05bd845ede3b61666e18ffcb64ea9d
estherkdy <73446024+estherkdy@users.noreply.github.com>
2024-10-31 16:05:32 -0400

    Update ClientLogin.js
    updated client login

--- database-frontend/src/pages/ClientLogin.js
+++ database-frontend/src/pages/ClientLogin.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+
 function ClientLogin() {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
@@ -14,18 +15,37 @@ function ClientLogin() {
     };
     const handleLogin = () => {
         // check for email and password in database
-        navigate('/clienthome')
-    }
+        navigate('/clienthome');
+    };
 
     const disabled = !email || !password;
 
     return (
-        <div>
-            <button title='Back' onClick={() => navigate(-1)}>Back</button>
+        <div className="login-container">
+            <button title="Back" onClick={() => navigate(-1)}>Back</button>
             <h1>Client Login</h1>
-            <input type='text' value={email} onChange={handleEmail} placeholder='email'></input>
-            <input type='text' value={password} onChange={handlePassword} placeholder='password'></input>
-            <button disabled={disabled} onClick={handleLogin}>Log In</button>
+            
+            <div className="input-row">
+                <input 
+                    type="text" 
+                    value={email} 
+                    onChange={handleEmail} 
+                    placeholder="Email"
+                />
+            </div>
+
+            <div className="input-row">
+                <input 
+                    type="password" 
+                    value={password} 
+                    onChange={handlePassword} 
+                    placeholder="Password"
+                />
+            </div>
+
+            <div className="input-row">
+                <button disabled={disabled} onClick={handleLogin}>Log In</button>
+            </div>
         </div>
     );
 }
