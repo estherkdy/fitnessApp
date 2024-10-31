77f05a1fc3d9ce60fa8e441ae9ac642201ffba89
estherkdy <73446024+estherkdy@users.noreply.github.com>
2024-10-31 16:12:40 -0400

    Update ClientHome.js

--- database-frontend/src/pages/ClientHome.js
+++ database-frontend/src/pages/ClientHome.js
@@ -10,7 +10,7 @@ function ClientHome() {
             <h1>Client Home Page</h1>
             <button>View Trainer Info</button> {/* view trainer's info */}
             <button>View Fitness Plan</button> {/* view your fitness plan */}
-            <button>Update Exeercise Status</button> {/* check off exercises completed */}
+            <button>Update Exercise Status</button> {/* check off exercises completed */}
             <button>Update Meal Status</button> {/* check off meals eaten */}
             <button>Check Reminders</button> {/* check reminders */}
             <button>Delete Account</button> {/* Delete all user info */}
