18b2db93444d2f2de40ba7eb171b506a74bdc1d3
estherkdy <73446024+estherkdy@users.noreply.github.com>
2024-11-18 15:42:26 -0500

    Update AdminHome.js
    setting up admin home page

--- database-frontend/src/pages/AdminHome.js
+++ database-frontend/src/pages/AdminHome.js
@@ -1,1 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './AdminHome.css'; // TODO
 
+function AdminHome() { 
+  // TODO
+}
+
+export default AdminHome;
